226b882d59aacc2f36d251ee15027b53bfb579b2
dragooon4ik24 <shafner.anton@gmail.com>
2020-09-27 23:21:56 +0300

    реализовал сортировку

--- src/utils/card.js
+++ src/utils/card.js
@@ -12,3 +12,30 @@ export const getFormattedDate = (date) =>
   `${date.getDate()} ${date.toLocaleString(`en-US`, {
     month: `long`,
   })} ${date.getFullYear()}`;
+
+export const sortRatingUp = (card1, card2) => {
+  if (card1.rating > card2.rating) {
+    return -1;
+  }
+
+  if (card1.rating < card2.rating) {
+    return 1;
+  }
+
+  return 0;
+};
+
+export const sortDateUp = (card1, card2) => {
+  const date1 = card1.release.date.getTime();
+  const date2 = card2.release.date.getTime();
+
+  if (date1 > date2) {
+    return -1;
+  }
+
+  if (date1 < date2) {
+    return 1;
+  }
+
+  return 0;
+};
--- src/view/sort.js
+++ src/view/sort.js
@@ -1,13 +1,43 @@
 import AbstractView from "./abstract";
+import {SortType} from "../const";
 
 const createSortTemplate = () => `<ul class="sort">
-  <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-  <li><a href="#" class="sort__button">Sort by date</a></li>
-  <li><a href="#" class="sort__button">Sort by rating</a></li>
+  <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+  <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+  <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
 </ul>`;
 
 export default class Sort extends AbstractView {
+  constructor() {
+    super();
+
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+    this._listLinks = null;
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (evt.target.tagName !== `A`) {
+      return;
+    }
+    for (let i = 0; i < this._listLinks.length; i++) {
+      if (this._listLinks[i].classList.contains(`sort__button--active`)) {
+        this._listLinks[i].classList.remove(`sort__button--active`);
+        break;
+      }
+    }
+    evt.target.classList.add(`sort__button--active`);
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._listLinks = this.getElement().querySelectorAll(`.sort__button`);
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener(`click`, this._sortTypeChangeHandler);
   }
 }
